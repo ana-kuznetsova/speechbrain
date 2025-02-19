2aad6e2ac8fff1be10f23fb11c700278ca30da40
Джамиля Эркенова <DM.Erkenova@ispras.ru>
2025-02-19 23:16:27 +0300

    added si-sdr

--- speechbrain/nnet/losses.py
+++ speechbrain/nnet/losses.py
@@ -14,6 +14,7 @@ import math
 from collections import namedtuple
 from itertools import permutations
 
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -1033,6 +1034,116 @@ def get_snr_with_pitwrapper(source, estimate_source):
 
     return loss
 
+def get_si_sdr_with_pitwrapper(source, estimate_source):
+    """This function wraps si_sdr calculation with the speechbrain pit-wrapper.
+
+    Arguments
+    ---------
+    source: torch.Tensor
+        Shape is [B, T, C],
+        Where B is the batch size, T is the length of the sources, C is
+        the number of sources the ordering is made so that this loss is
+        compatible with the class PitWrapper.
+    estimate_source: torch.Tensor
+        The estimated source, of shape [B, T, C]
+
+    Returns
+    -------
+    loss: torch.Tensor
+        The computed SDR
+
+    Example
+    -------
+    >>> x = torch.arange(600).reshape(3, 100, 2)
+    >>> xhat = x[:, :, (1, 0)]
+    >>> si_sdr = -get_si_sdr_with_pitwrapper(x, xhat)
+    >>> print(si_snr)
+    tensor([135.2284, 135.2284, 135.2284])
+    """
+
+    pit_si_sdr = PitWrapper(cal_si_sdr)
+    loss, perms = pit_si_sdr(source, estimate_source)
+
+    return loss
+
+
+def cal_si_sdr(source, estimate_source):
+    """Calculate SI-SDR.
+
+    Arguments
+    ---------
+    source: torch.Tensor
+        Shape is [T, B, C],
+        Where B is batch size, T is the length of the sources, C is the number of sources
+        the ordering is made so that this loss is compatible with the class PitWrapper.
+    estimate_source: torch.Tensor
+        The estimated source, of shape [T, B, C]
+
+    Returns
+    -------
+    The calculated SI-SDR.
+
+    Example:
+    ---------
+    >>> import numpy as np
+    >>> x = torch.Tensor([[1, 0], [123, 45], [34, 5], [2312, 421]])
+    >>> xhat = x[:, (1, 0)]
+    >>> x = x.unsqueeze(-1).repeat(1, 1, 2)
+    >>> xhat = xhat.unsqueeze(1).repeat(1, 2, 1)
+    >>> si_sdr = -cal_si_sdr(x, xhat)
+    >>> print(si_sdr)
+    tensor([[[ 25.2142, 144.1789],
+             [130.9283,  25.2142]]])
+    """
+    EPS = 1e-8
+    assert source.size() == estimate_source.size()
+    device = estimate_source.device.type
+
+    source_lengths = torch.tensor(
+        [estimate_source.shape[0]] * estimate_source.shape[-2], device=device
+    )
+    mask = get_mask(source, source_lengths)
+    estimate_source *= mask
+
+    num_samples = (
+        source_lengths.contiguous().reshape(1, -1, 1).float()
+    )  # [1, B, 1]
+    mean_target = torch.sum(source, dim=0, keepdim=True) / num_samples
+    mean_estimate = (
+        torch.sum(estimate_source, dim=0, keepdim=True) / num_samples
+    )
+    zero_mean_target = source - mean_target
+    zero_mean_estimate = estimate_source - mean_estimate
+    # mask padding position along T
+    zero_mean_target *= mask
+    zero_mean_estimate *= mask
+
+    # Step 2. SI-SNR with PIT
+    # reshape to use broadcast
+    s_target = zero_mean_target  # [T, B, C]
+    s_estimate = zero_mean_estimate  # [T, B, C]
+    # s_target = <s', s>s / ||s||^2
+    dot = torch.sum(s_estimate * s_target, dim=0, keepdim=True)  # [1, B, C]
+    s_target_energy = (
+        torch.sum(s_target**2, dim=0, keepdim=True) + EPS
+    )  # [1, B, C]
+    proj = dot * s_target / s_target_energy  # [T, B, C]
+    # e_noise = s' - s_target
+    # e_noise = s_estimate - proj  # [T, B, C]
+    alpha = torch.t(s_estimate) * s_target / s_target_energy
+    e_target = alpha * s_target
+    e_res = s_estimate - e_target
+    e_target_energy = (
+        torch.sum(e_target**2, dim=0, keepdim=True) + EPS
+    )
+    e_res_energy = (
+        torch.sum(e_res**2, dim=0, keepdim=True) + EPS
+    )
+    # SI-SNR = 10 * log_10(||s_target||^2 / ||e_noise||^2)
+    si_sdr_beforelog = e_target_energy / e_res_energy
+    si_sdr = 10 * torch.log10(si_sdr_beforelog + EPS)  # [B, C]
+
+    return -si_sdr.unsqueeze(0)
 
 def cal_si_snr(source, estimate_source):
     """Calculate SI-SNR.
